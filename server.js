95e1ddfab1a965799b3693bbac83539c9f1a25dd
mrlongruoi <mrlong01012020@gmail.com>
2025-06-02 20:07:15 +0700

    05: create backend server

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+// middleware
+app.use(cors());
+app.use(express.json());
+// routes
+app.get('/', (req, res) => res.send('API is Working'));
+const PORT = process.env.PORT | 3000;
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+});
+export default app;
