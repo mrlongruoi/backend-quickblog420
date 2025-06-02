58defd6b7f4de764aa747acd4ca05f351bb1b47e
mrlongruoi <mrlong01012020@gmail.com>
2025-06-02 23:14:36 +0700

    12: Create API for admin dashboard data

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -68,6 +68,8 @@ export const deleteBlogById = async (req, res) => {
     try {
         const {id} = req.body;
         await Blog.findByIdAndDelete(id);
+        // delete all comments associated with the blog
+        await Comment.deleteMany({blog: id});
         res.json({success: true, message: 'Blog deleted successfully'});
     } catch (error) {
         res.json({success: false, message: error.message});
